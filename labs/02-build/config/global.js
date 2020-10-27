e9e7ee5204daf34db97e3318c2e4d92b9c7ec2a3
pedreviljoen <pedre87@gmail.com>
2020-10-27 20:55:03 +0200

    Removed config from version control

--- labs/02-build/config/global.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    aws_dynamo_table: 'blogs',
-    aws_credentials: {
-        accessKeyId: 'ACCESS_KEY_ID',
-        secretAccessKey: 'SECRET_ACCESS_KEY',
-        region: 'eu-west-1',
-    }
-}
