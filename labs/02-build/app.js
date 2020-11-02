ec56825dc52c176238e9f60e2663300ca5577715
pedreviljoen <pedre87@gmail.com>
2020-11-02 14:06:19 +0200

    Updates  to API

--- labs/02-build/app.js
+++ labs/02-build/app.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const blog = require('./routes/blog')
-const PORT = 3000
+const music = require('./routes/music')
+const PORT = 5000
 
 // Create the express app instance
 const app = express()
 
 // Pass the JSON serializer and blogs routes to app
 app.use(bodyParser.json())
-app.use('/blogs', blog)
+app.use('/api/v1/music', music)
+
+// Health checker for ELB health checks
+app.get('/health', (req, res) => {
+    res.status(200).send('I am healthy')
+})
 
 // Let the express server listen on the defined PORT
-app.listen(PORT, `Listening on port: ${PORT}`)
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
--- labs/02-build/controllers/controller.js
+++ labs/02-build/controllers/controller.js
@@ -1,12 +1,62 @@
 const AWS = require('aws-sdk');
-const globalConf = require('../config/global')
+AWS.config.loadFromPath('/Users/pedreviljoen/Documents/Work/AWS/ci_cd_workshop/labs/02-build/config/config.json')
 
 // Instantiate dynamoDB instance - https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB.html
 const dynamoDB = new AWS.DynamoDB();
-AWS.config.update(globalConf.aws_credentials);
 
-exports.create = (req, res) => {
-    // TODO: use dynamoDB instance method putItem
+exports.create = async (req, res) => {
+    // Extract req paramaters
+    const {
+        album,
+        artist,
+        sales,
+        songs
+    } = req.body
+
+    // Build body for input operation
+    const params = {
+        Item: {
+            "Album": {
+                S: album
+            },
+            "Artist": {
+                S: artist
+            },
+            "Sales": {
+                N: sales
+            },
+            "Songs": {
+                N: songs
+            }
+        },
+        ReturnConsumedCapacity: "TOTAL",
+        TableName: "blogs"
+    }
+
+    // Call putItem method for DynamoDB SDK
+    const requestPromise = new Promise(async (resolve, reject) => {
+        dynamoDB.putItem(params, (err, data) => {
+            if (err) {
+                reject(err.stack)
+            }
+
+            resolve(data)
+        });
+    })
+
+    
+    try {
+        const request = await requestPromise
+
+        console.log('Response: ', request)
+
+        //
+    } catch (error) {
+        console.log('Error: ', error)
+        res.status(400).send({
+            error: error
+        })
+    }
 }
 
 exports.findOne = (req, res) => {
